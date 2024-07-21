de66b7ddf11b8ed3e37ba8d54a1171e893d56e89
Bezxc <bezxc@icloud.com>
2024-07-21 19:20:52 +0300

    Driver task

--- 6-driver/index.js
+++ 6-driver/index.js
@@ -2,4 +2,4 @@ const hasLicense = true;
 const age = 18;
 const isDrunk = false;
 
-const canDrive = hasLicense && age > 17 && !isDrunk ? "Может" : "Не может";
+const canDrive = hasLicense && age >= 18 && !isDrunk ? "Может" : "Не может";
